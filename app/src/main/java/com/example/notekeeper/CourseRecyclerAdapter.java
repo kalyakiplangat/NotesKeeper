e4b8470b52ec14cc9e93c25a6d33ddc5daaf327e
kalya <kalyakiplangat@gmail.com>
2019-09-03 22:41:34 +0300

    initialize repo

---
+++ app/src/main/java/com/example/notekeeper/CourseRecyclerAdapter.java
@@ -0,0 +1,63 @@
+package com.example.notekeeper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private final List<CourseInfo> mCourses;
+    private final LayoutInflater mLayoutInflater;
+
+    public CourseRecyclerAdapter(Context mContext, List<CourseInfo> course) {
+        this.mContext = mContext;
+        mLayoutInflater = LayoutInflater.from(mContext);
+        this.mCourses = course;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_course_list, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        CourseInfo course = mCourses.get(position);
+        viewHolder.mTextCourse.setText(course.getTitle());
+        viewHolder.mCurrentPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mTextCourse;
+        public  int mCurrentPosition;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, mCourses.get(mCurrentPosition).getTitle(), Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
